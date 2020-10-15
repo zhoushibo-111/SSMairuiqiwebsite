ce9e0754725f6d828d6a4ffc802a41dd97d84a93
zhoushiwangchao <xdxdxd1996@163.com>
2020-10-16 01:14:07 +0800

    增加介绍网页

--- src/main/java/com/sz/service/impl/UserServiceImpl.java
+++ src/main/java/com/sz/service/impl/UserServiceImpl.java
@@ -1,17 +1,16 @@
 package com.sz.service.impl;
 
-import com.sz.dto.UserDTO;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.sz.mapper.UserMapper;
+import com.sz.pojo.Product;
 import com.sz.pojo.User;
 import com.sz.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
 @Service("userService")
 public class UserServiceImpl implements UserService {
     @Autowired
@@ -22,14 +21,28 @@ public class UserServiceImpl implements UserService {
      * @return
      */
     @Override
-    public Map<Integer, User> queryUserDate() {
+    public PageInfo<User> queryUserDate(int pageNum, int pageSize) {
+        PageHelper.startPage(1, 5);
        List<User> list = userMapper.queryAll();
-       Map<Integer, User> map = new HashMap<>();
-       for (int i = 0; i < list.size(); i ++){
-           map.put(list.get(i).getId(), list.get(i));
-       }
+       //Map<Integer, User> map = new HashMap<>();
+//       for (int i = 0; i < list.size(); i ++){
+//           map.put(list.get(i).getId(), list.get(i));
+//       }
+        PageInfo<User> pageInfo = new PageInfo<>(list);
+        //return map;
+        return pageInfo;
+    }
 
-        return map;
+    @Override
+    public List<User> getlist(int start, int count) {
+        List<User> users = new ArrayList<>();
+        users = userMapper.getlist(start,count);
+        return users;
+    }
+
+    @Override
+    public Integer getTotal() {
+        return userMapper.getTotal();
     }
 
     @Override
