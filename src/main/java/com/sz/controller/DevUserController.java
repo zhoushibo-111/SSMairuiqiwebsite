816aa6d5ad38eccc9b8ebb22bf4c63ac5b736ddd
zhoushiwangchao <xdxdxd1996@163.com>
2020-10-26 00:41:09 +0800

    增加session 验证

--- src/main/java/com/sz/controller/DevUserController.java
+++ src/main/java/com/sz/controller/DevUserController.java
@@ -5,7 +5,6 @@ import com.sz.service.DevUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
@@ -19,8 +18,6 @@ public class DevUserController {
 
     @Autowired
     private DevUserService devUserService;
-
-
 
 
     @RequestMapping("/toLogin")
