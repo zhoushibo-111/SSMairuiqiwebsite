afdc129e6f2912a348ff14032a36201c69727114
zhoushiwangchao <xdxdxd1996@163.com>
2020-11-21 18:27:18 +0800

    修复发邮件功能

--- src/main/java/com/sz/util/JavaMail.java
+++ src/main/java/com/sz/util/JavaMail.java
@@ -21,18 +21,18 @@ import javax.mail.internet.MimeMessage;
 public class JavaMail {
     public void sendMail(String name,String email,String content) throws MessagingException {
         Properties props = new Properties();
-        props.setProperty("mail.smtp.auth", "true");
-        props.put("mail.smtp.port","465");
-        props.setProperty("mail.smtp.socketFactory.port", "465");
-        props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.setProperty("mail.smtp.timeout", "200000");//设置链接超时
+        props.setProperty("mail.smtp.port", Integer.toString(25));//设置端口
+        props.setProperty("mail.smtp.socketFactory.port", Integer.toString(465));//设置ssl端口
         props.setProperty("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.setProperty("mail.transport.protocol", "smtp");
         props.put("mail.smtp.host","smtp.163.com");// smtp服务器地址
         Session session = Session.getInstance(props);
         session.setDebug(true);
 
         Message msg = new MimeMessage(session);
-        msg.setSubject("尊敬的"+name+" 您好");
+        msg.setSubject("尊敬的"+name+": 您好");
         msg.setText(content+"\r\n"+"(根据您的留言,稍后我们会与您取得联系)");
         msg.setFrom(new InternetAddress("xdxdxd1996@163.com"));//发件人邮箱(我的163邮箱)
         msg.setRecipient(Message.RecipientType.TO,
